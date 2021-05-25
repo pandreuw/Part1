7d640093ee85083ff2382084d1a054d18801a497
Gerardo Moreno <wanderup.allego@gmail.com>
2021-05-25 07:48:44 -0400

    Exercise 1.6 unicafe step 1 finalized

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Display = ({text, value}) => <div>{text}{value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      {/* <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} /> */}
+      <h2>Statistics</h2>
+      <Display text="Good " value={good} />
+      <Display text="Neutral " value={neutral} />
+      <Display text="Bad " value={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
